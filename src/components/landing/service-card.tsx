dfbb2e1ccac0c3c9b156ce50ffa244ef95ea38da
Hamza <hamzaelbadaoui000@gmail.com>
2025-08-13 08:47:41 +0000

    border of services

--- src/components/landing/service-card.tsx
+++ src/components/landing/service-card.tsx
@@ -10,7 +10,7 @@ interface ServiceCardProps {
 
 export function ServiceCard({ img, hint, title, text }: ServiceCardProps) {
   return (
-    <Card className="overflow-hidden group hover:shadow-xl transition-shadow duration-300 border-none bg-card/50 text-left">
+    <Card className="overflow-hidden group hover:shadow-xl transition-shadow duration-300  bg-card/50 text-left">
       <CardHeader className="p-0">
         <Image src={img} alt={title} width={400} height={240} className="w-full h-40 object-cover group-hover:scale-105 transition-transform duration-300" data-ai-hint={hint} />
       </CardHeader>
